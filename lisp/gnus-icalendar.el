55c3d64ae55ba8df359a853f0fb52d24f041c981
Jan Tatarik <jan.tatarik@gmail.com>
2013-05-05 22:23:00 +0200

    include simple setup instructions

--- lisp/gnus-icalendar.el
+++ lisp/gnus-icalendar.el
@@ -20,7 +20,17 @@
 
 ;;; Commentary:
 
-;; 
+;; (require 'gmm-utils) ;; gmm-flet not in emacs
+                                        ;; proper yet
+;; (require 'gnus-icalendar)
+;; (gnus-icalendar-setup)
+
+;; to enable optional iCalendar->Org sync functionality
+;; NOTE: both the capture file and the headline(s) inside must already exist
+;; (setq gnus-icalendar-org-capture-file "~/org/notes.org")
+;; (setq gnus-icalendar-org-capture-headline '("Calendar"))
+;; (gnus-icalendar-org-setup)
+
 
 ;;; Code:
 
